a07a7759c59be80fb79a2917bba6fbc45d55bc2c
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-04-22 15:13:41 +0800

    added enum antype is lost

--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -1,9 +1,14 @@
 import { publicProcedure, router } from "./trpc";
 import { z } from "zod";
 
+function getEnum() {
+  return Math.random() > 0.5 ? 'a' : 'b'
+}
+
 export const appRouter = router({
   version: publicProcedure.query(() => ({
-    version: ""
+    version: "",
+    enum: getEnum()
   }))
 });
 
