2c712bef3480cbed85546d3719dcfb9bb15d4a70
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-04-22 15:30:15 +0800

    native enum works too

--- apps/nuxt/app.vue
+++ apps/nuxt/app.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import {version} from '#imports'
+import { version } from "#imports";
+import { GirlName } from "trpc";
 
 async function start() {
   // again it is correct, thanks to strict definition of result in trpc/src/index.ts
   // const v: {version: string, enum: "a" | "b"}
   const v = await version({
-    name: 'Triss'
+    name: 'Triss',
+    nativeEnum: GirlName.Triss
   });
 }
 </script>
--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -5,9 +5,17 @@ function getEnum() {
   return Math.random() > 0.5 ? 'a' : 'b'
 }
 
+export enum GirlName {
+  Triss,
+  Yennefer
+}
+
 export const appRouter = router({
   version: publicProcedure
-    .input(z.object({ name: z.enum(['Yennefer', 'Triss']) }))
+    .input(z.object({
+      name: z.enum(['Yennefer', 'Triss']),
+      nativeEnum: z.nativeEnum(GirlName)
+    }))
     .query((): {enum: 'a' | 'b', version: string} => ({
     version: "",
     enum: getEnum()
